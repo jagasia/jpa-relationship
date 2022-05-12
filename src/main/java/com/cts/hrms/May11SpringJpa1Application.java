44845373bb3a5a786ed94fab4ea6bddd7d7c501b
Jagadeeswaran <rjagadeeswaran@yahoo.com>
2022-05-12 10:08:33 +0530

    first

---
+++ src/main/java/com/cts/hrms/May11SpringJpa1Application.java
@@ -0,0 +1,23 @@
+package com.cts.hrms;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.cts.hrms.entity.Department;
+import com.cts.hrms.service.DepartmentService;
+
+@SpringBootApplication
+public class May11SpringJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(May11SpringJpa1Application.class, args);
+		System.out.println("Hello world");
+		
+		
+		
+	}
+
+}
